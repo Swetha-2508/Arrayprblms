2a599bb32781a6bbd139b7e52f0923af475d951c
Swetha <163889342+Swetha-2508@users.noreply.github.com>
2025-03-12 20:00:38 +0530

    Update and rename Arraymin.java to Arrayminmax.java

---
+++ Arrayminmax.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class arrayminmax
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+    int arr[]=new int[size];
+    for(int i=0;i<size;i++)
+    {
+      arr[i]=sc.nextInt();
+    }
+    int max=arr[0];
+    int min=arr[0];
+    for(int i=0;i<size;i++)
+    {
+      if(max<arr[i])
+      {
+        max=arr[i];
+      }
+      if(min>arr[i])
+      {
+        min=arr[i];
+      }
+    }
+    System.out.println("Maximum: " +max);
+    System.out.println("Minimum: " +min);
+  }   
+}
