29c3d3225b0a71768043864038e4a884b675ba36
Swetha <163889342+Swetha-2508@users.noreply.github.com>
2025-03-16 20:41:50 +0530

    Update and rename Arraysort.java to Arraylettersort.java

--- Arraylettersort.java
+++ Arraylettersort.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Arraysort {
+class Arraylettersort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        String arr[]=new String[a+1];
+        int size=sc.nextInt();
+        String arr[]=new String[size];
         for(int i=0;i<arr.length;i++)
         {
-            arr[i]=sc.nextLine();
+            arr[i]=sc.next();
         }
         Arrays.sort(arr);
         for(int i=0;i<arr.length;i++)
