5400c6c8b6d0ff2087fcb8473bff9dc6c667465e
Swetha <163889342+Swetha-2508@users.noreply.github.com>
2025-03-16 20:49:08 +0530

    Create Arraysortwithoutfunction.java

---
+++ Arraysortwithoutfunction.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class arraysortwithoutfunction {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=0;j<arr.length-1-i;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int num:arr)
+        {
+            System.out.println(num);
+        }
+    }
+}
