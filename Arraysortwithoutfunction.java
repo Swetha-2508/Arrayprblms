69909217ba0fda09ff2f66a8426fc654a81ab9f9
Swetha <163889342+Swetha-2508@users.noreply.github.com>
2025-03-27 22:22:21 +0530

    Update Arraysortwithoutfunction.java

--- Arraysortwithoutfunction.java
+++ Arraysortwithoutfunction.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class arraysortwithoutfunction {
+public class Arraysortwithoutfunction {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int size=sc.nextInt();
